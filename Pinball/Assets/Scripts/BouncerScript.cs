82071d6ae51369f66814b3cd6e6694f576916528
Fran_MSI\Fran <shaoranhyku@gmail.com>
2018-02-08 20:34:30 +0100

    Actualizado gráficos y turbo.

---
+++ Pinball/Assets/Scripts/BouncerScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncerScript : MonoBehaviour {
+
+    public Texture texturaCambiar;
+
+    private Renderer render;
+    private Texture texturaOriginal;
+
+    // Use this for initialization
+    void Start()
+    {
+        render = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            texturaOriginal = render.material.GetTexture("_MainTex");
+            render.material.SetColor("_Color", Color.black);
+            render.material.SetTexture("_MainTex", texturaCambiar);
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            StartCoroutine(RestoreColor());
+        }
+    }
+
+    private IEnumerator RestoreColor()
+    {
+        yield return new WaitForSeconds(4);
+        render.material.SetTexture("_MainTex", texturaOriginal);
+    }
+}
---
+++ Pinball/Assets/Scripts/TurboScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurboScript : MonoBehaviour {
+
+    public float force = 3f;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * force);
+        }
+
+    }
+}
